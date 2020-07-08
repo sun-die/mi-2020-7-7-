8c57edc54f0020b8292c0b8d9a8d8137c4e9532b
sun_die <1983812885@qq.com>
2020-07-08 10:08:41 +0800

    2020/7/8  10:08 首次提交

---
+++ src/js/detalis.js
@@ -0,0 +1,30 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal', // 横向切换选项
+    loop: true, // 循环模式选项
+    effect : 'fade',//淡入淡出
+    speed:800,
+    autoplay: {
+      delay:3000,
+      stopOnLastSlide: false,
+      disableOnInteraction: true,
+    },//自动轮播
+    // 如果需要分页器
+    pagination :{
+      el: '.swiper-pagination',
+      clickable :true,
+    //   bulletClass : 'my-bullet',
+    //   bulletActiveClass: 'my-bullet-active',
+  },
+    
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+  })  
+  for(i=0;i<mySwiper.pagination.bullets.length;i++){
+      mySwiper.pagination.bullets[i].onclick=function(){
+      this.click();
+      };
+  }
